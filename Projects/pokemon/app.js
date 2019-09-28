7a44b692da6bd802f2038ff5d70909483518d74d
Unknown <TAdcox009@west-mec.org>
2019-09-28 13:05:42 -0700

    matching game

--- Projects/pokemon/app.js
+++ Projects/pokemon/app.js
@@ -10,8 +10,40 @@ fetch(`https://pokeapi.co/api/v2/${_condition}/${_pocketmonster}`)
     console.log(data.sprites)
     const {...img} = data.sprites;
     // console.log(img)
-    selector = document.querySelector('.flip-card-front');
-    pokeIMG = `<img src="${img.front_default}" class="flip-card-front">`;
+    selector = document.querySelector('#arceus');
+    pokeIMG = `<img src="${img.front_default}" id="arceus">`;
+    selector.innerHTML = pokeIMG;
+}
+    );
+
+    // pikachu
+var _condition = "pokemon"
+var _pocketmonster = "pikachu"
+
+fetch(`https://pokeapi.co/api/v2/${_condition}/${_pocketmonster}`)
+.then( res => res.json())
+.then(data => {
+    console.log(data.sprites)
+    const {...img} = data.sprites;
+    // console.log(img)
+    selector = document.querySelector('#pikachu');
+    pokeIMG = `<img src="${img.front_default}" id="pikachu">`;
+    selector.innerHTML = pokeIMG;
+}
+    );
+
+     // mew
+var _condition = "pokemon"
+var _pocketmonster = "mew"
+
+fetch(`https://pokeapi.co/api/v2/${_condition}/${_pocketmonster}`)
+.then( res => res.json())
+.then(data => {
+    console.log(data.sprites)
+    const {...img} = data.sprites;
+    // console.log(img)
+    selector = document.querySelector('#mew');
+    pokeIMG = `<img src="${img.front_default}" id="mew">`;
     selector.innerHTML = pokeIMG;
 }
     );
